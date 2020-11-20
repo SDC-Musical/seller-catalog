47819bdd5e220d9f76f70ffd881436ee3bfc132d
grahse <gyykim@gmail.com>
2020-11-19 19:13:08 -0800

    Set up SQL schema for prices and sellers

---
+++ mysql-db/schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS buying_options;
+
+USE buying_options;
+
+CREATE TABLE prices (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT NOT NULL,
+  seller INT NOT NULL REFERENCES sellers(id),
+  price INT NOT NULL,
+  tax INT NOT NULL
+);
+
+CREATE TABLE sellers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  seller_name TEXT NOT NULL,
+  return_policy TEXT DEFAULT NULL,
+  delivery_free BOOLEAN NOT NULL,
+  delivery_min INT NOT NULL,
+  delivery_days INT NOT NULL,
+  delivery_fee INT NOT NULL
+);
